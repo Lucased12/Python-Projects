51d25330220e11299a4804f7468f29cf325e7517
Lucas Ducamp <leducamp@gmail.com>
2024-04-16 10:43:57 -0300

    Add files via upload

---
+++ Scripts/generarContrasenia.py
@@ -0,0 +1,21 @@
+import random
+import string
+
+def generarContrasenia(cantidadCaracteres):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    contrasenia = ''.join(random.choice(caracteres) for _ in range(cantidadCaracteres))
+    return contrasenia
+
+def main():
+    try:
+        longitudMaxima = int(input("Ingresa el numero de caracteres maximos para la contrasenia: "))
+        if longitudMaxima <= 0:
+            print("Poner un numero positivo. >:/")
+            return
+        contraseniaGenerada = generarContrasenia(longitudMaxima)
+        print("Contrasenia generada:", contraseniaGenerada)
+    except ValueError:
+        print("Hubo un error, intentar nuevamente!")
+
+if __name__ == "__main__":
+    main()
---
+++ Scripts/traductor.py
@@ -0,0 +1,43 @@
+from tkinter import Tk, Label, Entry, Button
+from googletrans import Translator
+from PIL import Image, ImageTk
+
+#Traducir el texto ingresado
+def traducirTexto():
+    #Obtener el texto ingresado por el usuario
+    textoEntrada = entradaTexto.get()
+
+    #Traducir el texto
+    traductor = Translator()
+    textoTraducido = traductor.translate(textoEntrada, dest='en').text
+
+    #Mostrar la traducción en la etiqueta
+    etiquetaSalida.config(text="Traducción: " + textoTraducido)
+
+#Funcion para cerrar la aplicación
+def cerrarApp():
+    raiz.destroy()
+
+#Configuracion de la interfaz gráfica
+raiz = Tk()
+raiz.title("Traductor")
+
+#Colores
+raiz.configure(bg='#2c3e50')
+colorTexto = '#ecf0f1'
+
+#Etiqueta y entrada para el texto que se va a traducir
+etiquetaEntrada = Label(raiz, text="Texto a Traducir:", bg='#2c3e50', fg=colorTexto)
+etiquetaEntrada.pack()
+entradaTexto = Entry(raiz, width=50)
+entradaTexto.pack()
+
+#Boton para iniciar la traducción
+botonTraducir = Button(raiz, text="Traducir", command=traducirTexto, bg='#34495e', fg=colorTexto)
+botonTraducir.pack(side='left')
+
+#Etiqueta para mostrar la traducción
+etiquetaSalida = Label(raiz, text="", bg='#2c3e50', fg=colorTexto)
+etiquetaSalida.pack()
+
+raiz.mainloop()
